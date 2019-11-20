e46a2f06e5b7cb33ad2d27937c565871037e5585
paopao <2906381723@qq.com>
2019-11-20 22:57:52 +0800

    注册中心Consul

--- mango-backup/src/main/java/com.paopao.mango.backup/MangoBackupApplication.java
+++ mango-backup/src/main/java/com.paopao.mango.backup/MangoBackupApplication.java
@@ -2,10 +2,12 @@ package com.paopao.mango.backup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Create by paopao on 2019-11-20 15:39
  */
+@EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = {"com.paopao.mango"})
 public class MangoBackupApplication {
 
--- mango-monitor/src/main/java/com/paopao/mango/monitor/MangoMonitorApplication.java
+++ mango-monitor/src/main/java/com/paopao/mango/monitor/MangoMonitorApplication.java
@@ -3,11 +3,13 @@ package com.paopao.mango.monitor;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Create by paopao on 2019-11-20 20:07
  */
 @EnableAdminServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MangoMonitorApplication {
 
